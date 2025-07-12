6ad9f0a48cd35780e843cf83aa0856bdfd31d127
alexpado <contact@alexpado.fr>
2025-07-12 05:00:53 +0200

    feat(media): Add MPD patching for labels
    It will use each media stream title in the metadata to patch the MPD file and add labels to each AdaptationSets

--- src/main/java/fr/anisekai/wireless/api/media/bin/wrapper/tasks/MpdTask.java
+++ src/main/java/fr/anisekai/wireless/api/media/bin/wrapper/tasks/MpdTask.java
@@ -7,20 +7,28 @@ import fr.anisekai.wireless.api.media.bin.wrapper.FFMpegCommand;
 import fr.anisekai.wireless.api.media.bin.wrapper.FFMpegCommandTask;
 import fr.anisekai.wireless.utils.FileUtils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Specific implementation {@link FFMpegCommand} allowing to convert a {@link MediaFile} into chunks with a DASH meta file.
  */
 public class MpdTask extends FFMpegCommandTask<Path> {
 
-    private final MediaFile input;
-    private final Path      output;
-    private final Path      mdp;
+    private final MediaFile         input;
+    private final Path              output;
+    private final Path              mdp;
+    private final List<MediaStream> adaptionSetsMedia;
 
     /**
      * Create a new {@link MpdTask} instance.
@@ -35,9 +43,10 @@ public class MpdTask extends FFMpegCommandTask<Path> {
     public MpdTask(MediaFile input, Path output, Path mdp) {
 
         super(Binary.ffmpeg());
-        this.input  = input;
-        this.output = output;
-        this.mdp    = mdp;
+        this.input             = input;
+        this.output            = output;
+        this.mdp               = mdp;
+        this.adaptionSetsMedia = new ArrayList<>();
     }
 
     @Override
@@ -53,7 +62,8 @@ public class MpdTask extends FFMpegCommandTask<Path> {
             switch (stream.getCodec().getType()) {
                 case VIDEO, AUDIO:
                     ffmpeg.addArguments("-map", "0:%s".formatted(stream.getId()));
-                    adaptationSets.add("id=%s,streams=%s".formatted(id, stream.getId()));
+                    adaptationSets.add("id=%s,streams=%s".formatted(id, id));
+                    this.adaptionSetsMedia.add(stream);
                     id++;
                     break;
             }
@@ -70,6 +80,49 @@ public class MpdTask extends FFMpegCommandTask<Path> {
 
     @Override
     public Path postprocess(int code) throws IOException {
+
+        Pattern pattern = Pattern.compile("<AdaptationSet id=\"(?<idx>\\d+)\"");
+
+        Path temp = Files.createTempFile("patched-", ".mpd");
+
+        try (
+                BufferedReader reader = Files.newBufferedReader(this.mdp);
+                BufferedWriter writer = Files.newBufferedWriter(temp, StandardOpenOption.TRUNCATE_EXISTING)
+        ) {
+            String line;
+
+            //noinspection NestedAssignment
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+
+                if (matcher.find()) {
+                    int idx = Integer.parseInt(matcher.group("idx"));
+
+                    if (idx >= 0 && idx < this.adaptionSetsMedia.size()) {
+                        MediaStream stream = this.adaptionSetsMedia.get(idx);
+
+                        if (stream.getMetadata().containsKey("title")) {
+                            String label = stream.getMetadata().get("title")
+                                                 .replace("&", "&amp;")
+                                                 .replace("\"", "&quot;")
+                                                 .replace("<", "&lt;")
+                                                 .replace(">", "&gt;");
+
+                            line = line.replaceFirst(
+                                    "id=\"%d\"".formatted(idx),
+                                    "id=\"%d\" label=\"%s\"".formatted(idx, label)
+                            );
+                        }
+                    }
+                }
+
+                writer.write(line);
+                writer.newLine();
+            }
+        }
+
+        // Atomically replace original MPD
+        Files.move(temp, this.mdp, StandardCopyOption.REPLACE_EXISTING);
 
         return this.mdp;
     }
